92cb6d7103debfda89d27934b60d43a287fdd54e
jesse zenon <jesse.zenon@gmail.com>
2022-06-08 20:16:35 -0500

    commit tree 0f0a589b3a979d092218b721c81f59aa7ef7099c

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,9 @@
-# Make your shoe class here!
+class Shoe
+    def initialized(brand, color, size, material, condition)
+        @color = color
+        @brand = brand
+        @size = size
+        @material = material
+        @condition = condition
+    end
+end
